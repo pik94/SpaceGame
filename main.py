a92b9a72ee826b78aff20ff4d3a6c6e95b8588bc
pik94 <pinchuk.ilia.work@gmail.com>
2021-01-17 14:45:34 +0300

    Added a twinkling star

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
-import time
 import curses
+import time
+
+
+def twinkle(canvas, row=5, column=20, pause=2, flag=None):
+    if flag:
+        canvas.addstr(row, column, '*', flag)
+    else:
+        canvas.addstr(row, column, '*')
+    canvas.refresh()
+    time.sleep(pause)
 
 
 def draw(canvas):
+    curses.curs_set(False)
     row, column = (5, 20)
-    canvas.addstr(row, column, 'Hello, World!')
     canvas.border()
-    canvas.refresh()
-    time.sleep(1)
+    while True:
+        twinkle(canvas, row, column, pause=2, flag=curses.A_DIM)
+        twinkle(canvas, row, column, pause=0.3)
+        twinkle(canvas, row, column, pause=0.5, flag=curses.A_BOLD)
+        twinkle(canvas, row, columnpause=0.3)
 
 
 if __name__ == '__main__':
