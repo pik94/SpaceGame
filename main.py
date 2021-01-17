8eeda9621308985bf0eff28e93a39e9340f3b8c4
pik94 <pinchuk.ilia.work@gmail.com>
2021-01-17 15:48:40 +0300

    Several stars. Each star is blinked own times

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 import curses
+import time
 
 import asyncio
 
 
-async def blink(canvas, row=5, column=10, symbol='*'):
-    while True:
+TIC_TIMEOUT = 0.1
+
+
+async def blink(canvas, row=5, column=10, times=3, symbol='*'):
+    for _ in range(0, times):
         canvas.addstr(row, column, symbol, curses.A_DIM)
         await asyncio.sleep(0)
 
@@ -23,10 +27,9 @@ def draw(canvas):
     canvas.border()
 
     coroutines = [
-        blink(canvas, 1, col)
+        blink(canvas, 1, col, times=col)
         for col in range(1, 11, 2)
     ]
-
     while True:
         for coroutine in coroutines.copy():
             try:
@@ -34,6 +37,9 @@ def draw(canvas):
                 canvas.refresh()
             except StopIteration:
                 coroutines.remove(coroutine)
+        if not coroutines:
+            break
+        time.sleep(TIC_TIMEOUT)
 
 
 if __name__ == '__main__':
