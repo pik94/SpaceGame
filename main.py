166e71bd5f7d38b1ef5dc63b1653a931856c2d23
pik94 <pinchuk.ilia.work@gmail.com>
2021-01-17 17:06:05 +0300

    Added a fire shot

--- main.py
+++ main.py
@@ -12,6 +12,40 @@ N_SAMPLES = 100
 BLINK_TIMES = 100
 
 
+async def fire(canvas,
+               start_row: int,
+               start_column: int,
+               rows_speed: Optional[float] = -0.3,
+               columns_speed: Optional[int] = 0):
+    """Display animation of gun shot, direction and speed can be specified."""
+
+    row, column = start_row, start_column
+
+    canvas.addstr(round(row), round(column), '*')
+    await asyncio.sleep(0)
+
+    canvas.addstr(round(row), round(column), 'O')
+    await asyncio.sleep(0)
+    canvas.addstr(round(row), round(column), ' ')
+
+    row += rows_speed
+    column += columns_speed
+
+    symbol = '-' if columns_speed else '|'
+
+    rows, columns = canvas.getmaxyx()
+    max_row, max_column = rows - 1, columns - 1
+
+    curses.beep()
+
+    while 1 < row < max_row and 1 < column < max_column:
+        canvas.addstr(round(row), round(column), symbol)
+        await asyncio.sleep(0)
+        canvas.addstr(round(row), round(column), ' ')
+        row += rows_speed
+        column += columns_speed
+
+
 async def sleep(seconds: Union[float, int] = 0):
     if not seconds:
         seconds = random.randint(1, 10)
@@ -61,6 +95,7 @@ def draw(canvas):
                                    symbol=random.choice(STARS))
 
     coroutines = list(coroutines.values())
+    coroutines.append(fire(canvas, y_max, x_max // 2))
     while True:
         for coroutine in coroutines.copy():
             try:
