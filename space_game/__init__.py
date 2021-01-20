7166102b0fd4bfabba225e0ffc7a2cc94cf5a996
pik94 <pinchuk.ilia.work@gmail.com>
2021-01-20 21:11:33 +0300

    Added a rubbish on the sky

---
+++ space_game/__init__.py
@@ -0,0 +1,1 @@
+from space_game.space_game import SpaceGame
---
+++ space_game/settings.py
@@ -0,0 +1,17 @@
+TIC_TIMEOUT = 0.1
+
+
+class SkySettings:
+    STAR_SET = ['+', '*', '.', ':']
+
+    # The higher the coefficient, the more stars on the sky.
+    # Should be more than 0
+    STAR_COEFF = 0.25
+
+
+class ContolSettings:
+    SPACE_KEY_CODE = 32
+    LEFT_KEY_CODE = 260
+    RIGHT_KEY_CODE = 261
+    UP_KEY_CODE = 259
+    DOWN_KEY_CODE = 258
